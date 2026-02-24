6e1d1a9f4b2cc4c3ada5901acf005346f7449ccc
yangcyyangcy <xyang8031@gmail.com>
2026-02-25 01:09:37 +0800

    V6: Fix outstatic Next 15 App router rendering

---
+++ app/api/outstatic/[[...ost]]/route.js
@@ -0,0 +1,9 @@
+import { OutstaticApi } from 'outstatic'
+
+export const GET = function (req, ctx) {
+    return OutstaticApi(req, ctx)
+}
+
+export const POST = function (req, ctx) {
+    return OutstaticApi(req, ctx)
+}
