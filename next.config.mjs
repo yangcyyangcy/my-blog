20cc45379974331d27d91538ea1c0f2f9f829034
yangcyyangcy <xyang8031@gmail.com>
2026-02-25 16:12:34 +0800

    feat(cms): update admin redirect to secret yancey.blog.outstatic path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/admin',
+        // 只有输入这串特定路径才能跳转到后台
+        source: '/yancey.blog.outstatic',
         destination: '/outstatic',
         permanent: false,
       },
