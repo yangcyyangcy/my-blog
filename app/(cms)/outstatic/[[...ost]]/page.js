ca93ee5b3d4e0ebb3ff7f99d43329bb92561e7ef
yangcyyangcy <xyang8031@gmail.com>
2026-02-25 14:53:36 +0800

    fix: revert dynamic import to restore Next 15 SSR build

--- app/(cms)/outstatic/[[...ost]]/page.js
+++ app/(cms)/outstatic/[[...ost]]/page.js
@@ -1,14 +1,9 @@
 import 'outstatic/outstatic.css'
 import { Outstatic } from 'outstatic'
-import dynamic from 'next/dynamic'
-
-const DynamicOstClient = dynamic(
-    () => import('outstatic/client').then(mod => mod.OstClient),
-    { ssr: false }
-)
+import { OstClient } from 'outstatic/client'
 
 export default async function Page(props) {
     const params = await props.params
     const ostData = await Outstatic()
-    return <DynamicOstClient ostData={ostData} params={params} />
+    return <OstClient ostData={ostData} params={params} />
 }
